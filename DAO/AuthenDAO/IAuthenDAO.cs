4f5521fb527e70bb286e6122e0b594a82eaa0002
QUANG NGUYỄN ĐỨC <20120359@student.hcmus.edu.vn>
2024-11-03 22:06:49 +0700

    fix: can not navigate after login

--- DAO/AuthenDAO/IAuthenDAO.cs
+++ DAO/AuthenDAO/IAuthenDAO.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Local_Canteen_Optimizer.Model;
+﻿using Local_Canteen_Optimizer.Model;
 using System.Threading.Tasks;
 
 namespace Local_Canteen_Optimizer.DAO.AuthenDAO
